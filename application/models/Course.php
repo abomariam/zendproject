9b8196972cfbc4ae8ba9e2cdfdb7ff5322f14c0a
manarmaklad <manarmohammed75@yahoo.com>
2015-02-23 22:19:10 +0200

    I created the models :)

---
+++ application/models/Course.php
@@ -0,0 +1,19 @@
+<?php
+
+class Application_Model_Course extends Zend_Db_Table_Abstract
+
+{
+    
+protected $_name='course';
+function addCourse($data){}
+function deleteCourse($id){}
+function updateCourse($data){}
+function getAllCourse(){}
+function getCourseById($id){}
+
+      
+        
+        
+
+}
+
---
+++ application/models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+class Application_Model_User extends Zend_Db_Table_Abstract
+{
+    protected $_name='user';
+    
+    function addUser($data){}
+    
+    function deleteUser($id){}
+    
+    function updateUser($data){}
+    
+    function getAllUsers(){}
+    
+    function getUserById($id){}
+    
+    
+
+}
+
