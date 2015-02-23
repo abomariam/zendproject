ba7190dd82c99bea097e6be90413a3946f046e48
abomariam <abo_mariam1@hotmail.com>
2015-02-23 19:30:15 +0200

    add the forms

---
+++ application/forms/CourseForm.php
@@ -0,0 +1,13 @@
+<?php
+
+class Application_Form_CourseForm extends Zend_Form
+{
+
+    public function init()
+    {
+        /* Form Elements & Other Definitions Here ... */
+    }
+
+
+}
+
---
+++ application/forms/RequestForm.php
@@ -0,0 +1,13 @@
+<?php
+
+class Application_Form_RequestForm extends Zend_Form
+{
+
+    public function init()
+    {
+        /* Form Elements & Other Definitions Here ... */
+    }
+
+
+}
+
