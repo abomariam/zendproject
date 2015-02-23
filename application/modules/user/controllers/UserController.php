590ad9d7554f4c3204ba9ed1fe3dc3348ed5a22a
Manamenoo <mohashem92@gmail.com>
2015-02-23 20:02:10 +0200

    creation of controllers&actions

---
+++ application/modules/user/controllers/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+class User_UserController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function registerAction()
+    {
+        // action body
+    }
+
+    public function loginAction()
+    {
+        // action body
+    }
+
+    public function editAction()
+    {
+        // action body
+    }
+
+    public function deleteAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
