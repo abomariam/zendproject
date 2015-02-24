8653cdd3b8d33aa693aebbd91e64938945d031b6
manarmaklad <manarmohammed75@yahoo.com>
2015-02-24 17:35:05 +0200

    I use this in functions

--- application/models/Comment.php
+++ application/models/Comment.php
@@ -5,23 +5,23 @@ class Application_Model_Comment extends Zend_Db_Table_Abstract {
     protected $_name = 'comment';
 
     function addComment($data) {
-        
+        $this->insert($data);
     }
 
     function deleteComment($id) {
-        
+        $this->delete("id=$id");
     }
 
     function updateComment($data) {
-        
+        $this->update($data, 'id=' . $data['id']);
     }
 
     function getAllComment() {
-        
+        return $this->fetchAll()->toArray();
     }
 
     function getCommentById($id) {
-        
+        $array = $this->find($id)->toArray();
     }
 
 }
