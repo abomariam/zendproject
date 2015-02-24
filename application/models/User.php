e86207ff8f96972e5b6e6e2fb66a1bdd9aabd907
manarmaklad <manarmohammed75@yahoo.com>
2015-02-24 22:17:13 +0200

    I created the body of the  functions

--- application/models/User.php
+++ application/models/User.php
@@ -1,20 +1,32 @@
 <?php
 
-class Application_Model_User extends Zend_Db_Table_Abstract
-{
-    protected $_name='user';
-    
-    function addUser($data){}
-    
-    function deleteUser($id){}
-    
-    function updateUser($data){}
-    
-    function getAllUsers(){}
-    
-    function getUserById($id){}
-    
-    
+class Application_Model_User extends Zend_Db_Table_Abstract {
+
+    protected $_name = 'user';
+
+    function addUser($data) {
+        $this->insert($data);
+        
+    }
+
+    function deleteUser($id) {
+         $this->delete("id=$id");
+        
+    }
+
+    function updateUser($data) {
+        $this->update($data, 'id=' . $data['id']);
+        
+        
+    }
+
+    function getAllUsers() {
+        return $this->fetchAll()->toArray();
+        
+    }
+
+    function getUserById($id) {
+        $array = $this->find($id)->toArray();
+    }
 
 }
-
