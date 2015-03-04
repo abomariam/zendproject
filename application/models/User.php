2210151c5d197bcc97e08570be5aa3b2e21ae58d
manarmaklad <manarmohammed75@yahoo.com>
2015-03-04 19:00:22 +0200

    I linked the tables with each other by id

--- application/models/User.php
+++ application/models/User.php
@@ -5,6 +5,7 @@ class Application_Model_User extends Zend_Db_Table_Abstract {
     protected $_name = 'user';
 
     function addUser($data) {
+        $data["password"]=md5($data["password"]);
         $this->insert($data);
         
     }
