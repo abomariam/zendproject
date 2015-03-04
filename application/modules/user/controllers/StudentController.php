372f2dcccfb35de67ca8299f8bc53c61d7078036
Manamenoo <mohashem92@gmail.com>
2015-03-04 16:41:19 +0200

    adding actions to student controller

--- application/modules/user/controllers/StudentController.php
+++ application/modules/user/controllers/StudentController.php
@@ -15,12 +15,43 @@ class User_StudentController extends Zend_Controller_Action
 
     public function registerAction()
     {
-        // action body
+        $this->view->page_title = "Registration";
+        $user_form = new Application_Form_UserDetails();
+        
+        
+        if($this->getRequest()->isPost()){
+            if($user_form->isValid($_POST)){
+                $user_model = new Application_Model_User();
+                $this->view->success = $user_model->addUser($user_form->getValues());
+            }
+        }
+        
+        $this->view->form = $user_form;
     }
 
     public function loginAction()
     {
-        // action body
+        if($this->getRequest()->isPost()){
+            $data = $this->getRequest()->getParams();
+            $db = Zend_Db_Table::getDefaultAdapter();
+            
+            $authAdapter = new Zend_Auth_Adapter_DbTable($db, 'user','email', 'password');
+            $authAdapter->setIdentity($data['email']);
+            $authAdapter->setCredential(md5($data['password']));
+            $result = $authAdapter->authenticate();
+            if ($result->isValid()) {
+            
+            $auth = Zend_Auth::getInstance();
+            $storage = $auth->getStorage();
+            $storage->write($authAdapter->getResultRowObject(
+            array('email' , 'id' , 'full_name', 'role')));
+            
+            return $this->_redirect('post/list');
+            } else {
+            $this->view->loginMessage = "Sorry, your username or password was incorrect";
+            $this->_forward('index','index');
+            }
+        }
     }
 
     public function editAction()
@@ -32,15 +63,11 @@ class User_StudentController extends Zend_Controller_Action
     {
         // action body
     }
-
-
+    
+     public function logoutAction()
+    {
+        $authAdapter = Zend_Auth::getInstance();
+        $authAdapter->clearIdentity();
+        $this->_redirect("index/index");
+    }
 }
-
-
-
-
-
-
-
-
-
