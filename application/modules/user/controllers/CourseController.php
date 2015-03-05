bed4fde45e43213cff9edad3f91c64e32d53ddf7
Manamenoo <mohashem92@gmail.com>
2015-03-05 18:09:39 +0200

    finish login,register,logout actions in student controller ^_^

--- application/modules/user/controllers/CourseController.php
+++ application/modules/user/controllers/CourseController.php
@@ -1,6 +1,6 @@
 <?php
 
-class User_MaterialController extends Zend_Controller_Action
+class User_CourseController extends Zend_Controller_Action
 {
 
     public function init()
--- application/modules/user/controllers/StudentController.php
+++ application/modules/user/controllers/StudentController.php
@@ -5,7 +5,10 @@ class User_StudentController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        $auth = Zend_Auth::getInstance();
+        if ($auth->hasIdentity()) {
+            $this->view->user=$auth->getIdentity();            
+        }
     }
 
     public function indexAction()
@@ -31,29 +34,35 @@ class User_StudentController extends Zend_Controller_Action
 
     public function loginAction()
     {
+        $auth = Zend_Auth::getInstance();
+        if (!$auth->hasIdentity()) {
         $user_form = new Application_Form_UserLogin();
         if($this->getRequest()->isPost()){
             if($user_form->isValid($_POST)){
                 $data = $this->getRequest()->getParams();
                 $db = Zend_Db_Table::getDefaultAdapter();
-
                 $authAdapter = new Zend_Auth_Adapter_DbTable($db, 'user','email', 'password');
                 $authAdapter->setIdentity($data['email']);
                 $authAdapter->setCredential(md5($data['password']));
                 $result = $authAdapter->authenticate();
                 if ($result->isValid()) {
-
-                $auth = Zend_Auth::getInstance();
-                $storage = $auth->getStorage();
-                $storage->write($authAdapter->getResultRowObject(
-                array('email' , 'id' , 'full_name', 'role')));
-
-                return $this->_redirect('material/list');
-                } else {
-                $this->view->loginMessage = "Sorry, your username or password was incorrect";
-                }
-            }
+                    
+                    $storage = $auth->getStorage();
+                    $storage->write($authAdapter->getResultRowObject(array('id','name','email','gender' , 'signature','country' , 'pic','is_active','role')));              
+                    if($auth->getIdentity()->is_active){
+                        return $this->_redirect('material/list');
+                        }else{
+                            $authAdapter->clearIdentity();
+                            $this->view->loginMessage = "Sorry, your account isn't active ";
+                        }
+                    }else{
+                         $this->view->loginMessage = "Sorry, your username or password was incorrect";
+                        }
+            }    
         }
+        }else{
+                $this->_redirect("index/index");
+            }
         $this->view->form = $user_form;
     }
 
